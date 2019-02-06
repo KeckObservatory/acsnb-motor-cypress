f73b07f358c72ab2ed8c831c6050cb97b9319ca7
Paul Richards <prichards@keck.hawaii.edu>
2019-02-05 15:44:49 -1000

    Sync up with Paul's laptop version for release to acceptance test
    Clean build and remake on Paul's laptop and check-in to make sure everything synched up for use with acceptance test.

--- QuadEncoderRTOS.cydsn/codegentemp/QuadEncoderRTOS.v
+++ QuadEncoderRTOS.cydsn/codegentemp/QuadEncoderRTOS.v
@@ -1,6 +1,6 @@
 // ======================================================================
 // QuadEncoderRTOS.v generated from TopDesign.cysch
-// 12/26/2018 at 15:37
+// 02/05/2019 at 15:43
 // This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 // ======================================================================
 
--- QuadEncoderRTOS.cydsn/codegentemp/QuadEncoderRTOS_u.sdc
+++ QuadEncoderRTOS.cydsn/codegentemp/QuadEncoderRTOS_u.sdc
@@ -1,3 +1,3 @@
 # Component constraints for C:\proj\acsnb-motor-cypress\QuadEncoderRTOS.cydsn\TopDesign\TopDesign.cysch
 # Project: C:\proj\acsnb-motor-cypress\QuadEncoderRTOS.cydsn\QuadEncoderRTOS.cyprj
-# Date: Thu, 27 Dec 2018 01:37:57 GMT
+# Date: Wed, 06 Feb 2019 01:43:24 GMT
